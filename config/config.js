cf522f3b803ad3bbed74ab833fe163d92fab3327
matt silverstein <welcomematt@gmail.com>
2013-09-12 19:47:46 -0700

    testing socket.io and set production database for deployment

--- config/config.js
+++ config/config.js
@@ -27,7 +27,7 @@ var config = {
       name: 'mmofftest'
     },
     port: 3000,
-    db: 'mongodb://localhost/mmoff'
+    db: 'mongodb://nodejitsu_silversteez:s0f2jvums14appp6l3uof0ctg@ds039257.mongolab.com:39257/nodejitsu_silversteez_nodejitsudb8602391087'
   }
 };
 
--- config/socket.js
+++ config/socket.js
@@ -5,6 +5,13 @@ module.exports = function(ioObj) {
 
     socket.emit('news', { hello: 'world' });
 
+
+    var emitTester = function() {
+      socket.emit('test', { theTest: "it worked!" } );
+    };
+
+    setInterval(emitTester, 1000);
+
     socket.on('my other event', function (data) {
       console.log('"my other event" data: ', data);
     });
