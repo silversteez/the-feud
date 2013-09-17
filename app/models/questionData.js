f3b3b453aae0e1b009ef9c3bc37094b8c7d99d26
matt silverstein <welcomematt@gmail.com>
2013-09-16 17:32:01 -0700

    refactoring game logic to include multiple game states

--- app/models/questionData.js
+++ app/models/questionData.js
@@ -1,29 +1,35 @@
 module.exports = [
   {
-    question: "Why does President Obama have to look so _______ all the time?!"
+    question: 'Name something you would find in a forest.'
   },
   {
-    question: "I could really go for a ________ right now..."
+    question: 'Name a word that rhymes with "tank".'
   },
   {
-    question: "________ is pretty much the best cheese ever."
-  },
-  {
-    question: "If I could only eat one food for the rest of my life, it definitely would NOT be _______!"
-  },
-  {
-    question: "I'd love to have a dog named _______! That'd be super."
-  },
-  {
-    question: "Why do people always ______ on each other? I don't get it."
-  },
-  {
-    question: "I'd love to take that _______ for a ride!"
-  },
-  {
-    question: "Somebody ought to put a _______ in that thing!"
-  },
-  {
-    question: "If I only had a couple more ________! That would be fantastic!"
+    question: 'Name a type of flower.'
   }
-]
+  // {
+  //   question: "I could really go for a ________ right now..."
+  // },
+  // {
+  //   question: "________ is pretty much the best cheese ever."
+  // },
+  // {
+  //   question: "If I could only eat one food for the rest of my life, it definitely would NOT be _______!"
+  // },
+  // {
+  //   question: "I'd love to have a dog named _______! That'd be super."
+  // },
+  // {
+  //   question: "Why do people always ______ on each other? I don't get it."
+  // },
+  // {
+  //   question: "I'd love to take that _______ for a ride!"
+  // },
+  // {
+  //   question: "Somebody ought to put a _______ in that thing!"
+  // },
+  // {
+  //   question: "If I only had a couple more ________! That would be fantastic!"
+  // }
+];
--- config/socket.js
+++ config/socket.js
@@ -1,46 +1,118 @@
 var questions = require('../app/controllers/questions');
 
 module.exports = function(ioObj) {
-  //length in seconds of one game question loop
-  var gameCycle = 5;
-  //how often to broadcast an update
-  var gameBeat = 1000;
-  //where are we in the cycle
-  var cycleTime = 0;
+  var answers = [];
+  //store the current state
+  var gameState;
+  //cache the next game state whenever we setState()
+  var nextGameState;
 
-  //run this every gameBeat so we can track what second of the cycle we're on
-  //at the end of a cycle, do stuff
-  setInterval(function() {
-    cycleTime++;
-    if (cycleTime >= gameCycle) {
-      //broadcast new question to everyone
-      //hacked to use in-memory questions so don't need to queue up next question for now
-      var nextQuestion = questions.getNextQuestion();
-      ioObj.sockets.emit('nextQuestion', {
-        question: nextQuestion,
-        gameCycle: gameCycle
-      });
-      cycleTime = 0;
+  var state = {
+    question: "question",
+    answer: "answer",
+    transition: "transition"
+  };
+
+  var setState = function(st) {
+    console.log("state is ", st);
+
+    switch(st) {
+      case state.question:
+      console.log("entered case question");
+      cycleTime = questionTime;
+      cycleTotalTime = questionTime;
+      gameState = state.question;
+      nextGameState = state.transition;
+      enterQuestionState();
+      break;
+
+      case state.transition:
+      console.log("entered case transition");
+      cycleTime = transitionTime;
+      cycleTotalTime = transitionTime;
+      gameState = state.transition;
+      nextGameState = state.answer;
+      break;
+
+      case state.answer:
+      console.log("entered case answer");
+      cycleTime = answerTime;
+      cycleTotalTime = answerTime;
+      gameState = state.answer;
+      nextGameState = state.question;
+      break;
+
     }
-  }, gameBeat);
+  };
+
+  var enterQuestionState = function() {
+    //broadcast new question to everyone
+    //hacked to use in-memory questions so don't need to queue up next question for now
+    var nextQuestion = questions.getNextQuestion();
+    ioObj.sockets.emit('nextQuestion', {
+      question: nextQuestion,
+      cycleTotalTime: cycleTotalTime
+    });
+  };
+
+  var emitCycleUpdate = function() {
+    var updateObj = {
+      gameState: gameState,
+      cycleTotalTime: cycleTotalTime,
+      cycleTime: cycleTime,
+    };
+    //if we are in question state, add the current question to the update
+    if (gameState === state.question) {
+      updateObj.question = questions.getCurrentQuestion();
+    }
+    ioObj.sockets.emit('cycleUpdate', updateObj);
+  };
+
+  var questionTime = 15;
+  var transitionTime = 5;
+  var answerTime = 10;
+  //keep a reference to the total time of the current cycle to send to clients
+  var cycleTotalTime;
+  //where are we in the current cycle
+  var cycleTime;
+  //how often to broadcast an update
+  var updateBeat = 1000;
+
+  //set the starting game state
+  setState(state.question);
+
+  //run this every updateBeat so we can track what second of the cycle we're on
+  setInterval(function() {
+    console.log("cycleTime is: ", cycleTime);
+    cycleTime--;
+    if (cycleTime <= 0) {
+      setState(nextGameState);
+    }
+    //update switch
+    switch(gameState) {
+      case state.question:
+      break;
+      case state.answer:
+      break;
+      case state.transition:
+      break;
+    }
+
+    //send out a cycle update every updateBeat to keep clients synced up
+    emitCycleUpdate();
+    console.log(gameState);
+    console.log(cycleTotalTime);
+
+  }, updateBeat);
 
   //all the stuff we do that requires a connection
   ioObj.sockets.on('connection', function (socket) {
-    console.log('connection!');
+    //send an update immediately on connection to let client know what state game is in
+    emitCycleUpdate();
 
-    socket.on('getQuestion', function() {
-      var currentQuestion = questions.getCurrentQuestion();
-      socket.emit('currentQuestion', {
-        question: currentQuestion,
-        gameCycle: gameCycle,
-        cycleTimeRemaining: gameCycle - cycleTime
-      });
+    socket.on('submitAnswer', function(data) {
+      answers.push(data.answer);
     });
-    // var emitTester = function() {
-    //   socket.emit('test', { theTest: "it worked!" } );
-    //   console.log( questions.getNextQuestion());
-    // };
-    // setInterval(emitTester, 1000);
   });
 
 };
