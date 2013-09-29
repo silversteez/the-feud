3fa1cfe49005d3e34d68f2e589d15a13b0a7c66f
matt silverstein <welcomematt@gmail.com>
2013-09-28 17:45:17 -0700

    added more questions

--- app/questionData.js
+++ app/questionData.js
@@ -1,5 +1,29 @@
 module.exports = [
   {
+    question: "Name something that costs more money if you have a daughter instead of a son."
+  },
+  {
+    question: "Name something that some people just cannot hold onto."
+  },
+  {
+    question: "Name a word that most people yell at their dogs."
+  },
+  {
+    question: "Name a bad sport for someone who is afraid of the water."
+  },
+  {
+    question: "Name a beverage that is served at kids birthday party."
+  },
+  {
+    question: "Name something New York City claims to have the very best of."
+  },
+  {
+    question: "Name something you wear that might be plaid."
+  },
+  {
+    question: "Name something people have to run to catch."
+  },
+  {
     question: 'Name something you would find in a forest.'
   },
   {
@@ -7,29 +31,131 @@ module.exports = [
   },
   {
     question: 'Name a type of flower.'
+  },
+  {
+    question: "Name something that is harder to do if you have braces."
+  },
+  {
+    question: "Name something cats have that people do not."
+  },
+  {
+    question: "Name someplace you would find a water fountain."
+  },
+  {
+    question: "Name a type of fish people order at fine restaurants."
+  },
+  {
+    question: "Name something people wear when they want to dress casual."
+  },
+  {
+    question: "Name a reason why a baby might be cranky."
+  },
+  {
+    question: "Name something you might take with you for an overnight stay at the hospital."
+  },
+  {
+    question: "Name something people do with snow."
+  },
+  {
+    question: "Name a tiny creature which frightens big people."
+  },
+  {
+    question: "Name something people save."
+  },
+  {
+    question: "Name someplace where you might find lint."
+  },
+  {
+    question: "Name something people do to cool down on a hot summer day."
+  },
+  {
+    question: "Name a food that has a lot of fiber."
+  },
+  {
+    question: "Name something that is better when it is hot."
+  },
+  {
+    question: "Name something people do to get on their boss's good side."
+  },
+  {
+    question: "Name something that makes you itch."
+  },
+  {
+    question: "Name something that receives a grade."
+  },
+  {
+    question: "Name something you can find in a Denver omelet."
+  },
+  {
+    question: "Name something women get excited about."
+  },
+  {
+    question: "Name a reason why someone might start sweating."
+  },
+  {
+    question: "Name something people mistake for a U.F.O."
+  },
+  {
+    question: "Name the first food people learn how to cook."
+  },
+  {
+    question: "Name something you hold very carefully."
+  },
+  {
+    question: "Name something that is harder to do in the snow."
+  },
+  {
+    question: "Name a part of the body that gives you problems as you get older."
+  },
+  {
+    question: " Name one of the basic necessities of life."
+  },
+  {
+    question: "Name an animal that a zoo usually has a lot of."
+  },
+  {
+    question: "Name something people accidentally walk into."
+  },
+  {
+    question: "Name something you can see a lot of in a Las Vegas casino."
+  },
+  {
+    question: "Name something in an office that drives people crazy when it is not working."
+  },
+  {
+    question: "Name a job that requires a tuxedo."
+  },
+  {
+    question: "Which U.S. city would you most be afraid to live in?"
+  },
+  {
+    question: "Name something that married women might be sick of doing."
+  },
+  {
+    question: "Name something your parents still give you."
+  },
+  {
+    question: "Name something that used to be free that there is now a charge for."
+  },
+  {
+    question: "Name something people like to look at when they are bored."
+  },
+  {
+    question: "Name something specific that people make reservations for."
+  },
+  {
+    question: "Name a part of the body that frequently gets sunburned."
+  },
+  {
+    question: "Name a kind of muffin."
+  },
+  {
+    question: "Name an animal hikers fear running into."
+  },
+  {
+    question: "Name something that comes in nuggets."
+  },
+  {
+    question: "Name something people have to run to catch."
   }
-  // {
-  //   question: "I could really go for a ________ right now..."
-  // },
-  // {
-  //   question: "________ is pretty much the best cheese ever."
-  // },
-  // {
-  //   question: "If I could only eat one food for the rest of my life, it definitely would NOT be _______!"
-  // },
-  // {
-  //   question: "I'd love to have a dog named _______! That'd be super."
-  // },
-  // {
-  //   question: "Why do people always ______ on each other? I don't get it."
-  // },
-  // {
-  //   question: "I'd love to take that _______ for a ride!"
-  // },
-  // {
-  //   question: "Somebody ought to put a _______ in that thing!"
-  // },
-  // {
-  //   question: "If I only had a couple more ________! That would be fantastic!"
-  // }
 ];
